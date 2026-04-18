1f34e5bb3e3c9e76d437ff7d0db9a7ae5d8b04c2
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-04-18 12:14:07 +0200

    feat: printf

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        String nom = "Daniel";
+        int age = 20;
+        System.out.printf("salut ici! je m'appelle %s et j'ai %d ans%n", nom, age);
+    }
+}
