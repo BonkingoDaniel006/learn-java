742ca03e49806beea41c00710ae6fd1625b60329
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-04-18 13:00:00 +0200

    update/main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Main {
+/*public class Main {
     public static void main(String[] args){
         String nom = "Bokingo";
         String prenom = "Daniel";
@@ -6,6 +6,22 @@ public class Main {
         String presentations = "Salut! je suis " + nom + " "+ prenom + " et j'ai " + age + "ans";
         System.out.println(presentations);
 
+
+    }
+}*/
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("entrez votre nom: ");
+        String nom = sc.nextLine();
+        System.out.println("bonjour " + nom);
+        sc.close();
 
     }
 }
