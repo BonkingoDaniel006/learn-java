79b60e904b41ebb90945722a0275b67258fb0978
Daniel BOKINGO <bokingodaniel4@gmail.com>
2026-04-20 02:32:14 +0200

    update/ main.java

--- Main.java
+++ Main.java
@@ -73,3 +73,14 @@ public class Main {
         }
     }
 }*/
+
+
+public class Main {
+    public static void main (String[] args){
+        int number = 0;
+        while (number<5) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
